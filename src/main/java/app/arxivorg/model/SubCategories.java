5d355e5e9a25257603d4b2d7c04a07f7059a6a0e
tom <tom.david-broglio@etu.univ-amu.fr>
2020-03-12 16:48:41 +0100

    ajout des constructeur

--- src/main/java/app/arxivorg/model/SubCategories.java
+++ src/main/java/app/arxivorg/model/SubCategories.java
@@ -1,17 +1,31 @@
 package app.arxivorg.model;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class SubCategories {
     private ArrayList<String> subCategories;
-    public boolean add(String categories){
-        if (!subCategories.contains(categories)){
-            subCategories.add(categories);
+
+    public SubCategories (){
+        this.subCategories = new ArrayList<>();
+    }
+
+    public SubCategories (List<String> list){
+        this.subCategories = new ArrayList<>();
+        for(String s:list){
+            this.add(s);
+        }
+    }
+
+    public boolean add(String category){
+        if (!subCategories.contains(category)){
+            subCategories.add(category);
             return true;
         }
         return false;
     }
+
 
 }
