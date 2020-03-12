64635385c944fdafe2ac83c5875ae989ffa8d334
tom <tom.david-broglio@etu.univ-amu.fr>
2020-03-12 17:10:36 +0100

    Authors complété avec getter et add

--- src/main/java/app/arxivorg/model/Authors.java
+++ src/main/java/app/arxivorg/model/Authors.java
@@ -7,7 +7,22 @@ public class Authors {
     private ArrayList<String> data;
 
     public Authors(List<String> input) {
-        this.data = new ArrayList<>(input);
+        this.data = new ArrayList<>();
+        for(String s:input){
+            this.add(s);
+        }
+    }
+
+    public ArrayList<String> get() {
+        return data;
+    }
+
+    public boolean add(String author){
+        if(!data.contains(author)){
+            data.add(author);
+            return true;
+        }
+        return false;
     }
 
     public String toString() {
