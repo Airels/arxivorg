07aaaf9dd7c987c0b894fe551c1053069bc6579f
tom <tom.david-broglio@etu.univ-amu.fr>
2020-03-12 16:52:22 +0100

    ajout de Article et ses attribut

---
+++ src/main/java/app/arxivorg/model/Article.java
@@ -0,0 +1,9 @@
+package app.arxivorg.model;
+
+public class Article {
+    private String title;
+    private Authors authors;
+    private String Content;
+    private Category category;
+    private SubCategories subCategories;
+}
