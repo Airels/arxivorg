8cc962da324a609c92baef31fc2af4fd643256fd
tom <tom.david-broglio@etu.univ-amu.fr>
2020-03-12 17:44:52 +0100

    test add

--- src/test/java/app/arxivorg/model/SubCategoriesTest.java
+++ src/test/java/app/arxivorg/model/SubCategoriesTest.java
@@ -1,4 +1,13 @@
 package app.arxivorg.model;
 
+import org.junit.jupiter.api.Test;
+
 public class SubCategoriesTest {
+    private SubCategories subCategories = new SubCategories();
+
+    @Test
+    public void addTest(){
+        assert(subCategories.add("test"));
+        assert(!subCategories.add("test"));
+    }
 }
