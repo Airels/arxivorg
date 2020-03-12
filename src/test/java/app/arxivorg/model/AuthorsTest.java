1b491c33d812586dfd276eb8758bf664036c30ae
tom <tom.david-broglio@etu.univ-amu.fr>
2020-03-12 17:30:15 +0100

    test de get

--- src/test/java/app/arxivorg/model/AuthorsTest.java
+++ src/test/java/app/arxivorg/model/AuthorsTest.java
@@ -3,12 +3,15 @@ package app.arxivorg.model;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AuthorsTest {
 
     private Authors authors = new Authors(Arrays.asList("Martin Dupont", "Marie Martin", "François Cordonnier"));
+    private List<String> getTest = Arrays.asList("Martin Dupont", "Marie Martin", "François Cordonnier");
+    private List<String> getTest2 = Arrays.asList("Martin Dupot", "Marie Martin", "François Cordonnier");
 
     @Test
     public void testToString() {
@@ -20,6 +23,13 @@ public class AuthorsTest {
         assert (authors.add("test"));
         assert (!authors.add("Martin Dupont"));
     }
+    @Test
+    public void TestGet(){
+        assert (authors.get().equals(getTest));
+        assert (!authors.get().equals(getTest2));
+    }
+
+
 
 
 }
