3a3a82be6e84131a53a1583fca297de0932db9d9
Airels <yohan13240@hotmail.fr>
2020-03-15 21:17:55 +0100

    Génération élements listView implémenté

--- src/main/java/app/arxivorg/controller/ArxivOrgController.java
+++ src/main/java/app/arxivorg/controller/ArxivOrgController.java
@@ -1,35 +1,60 @@
 package app.arxivorg.controller;
 
+import app.arxivorg.model.Article;
+import app.arxivorg.model.Authors;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class ArxivOrgController implements Initializable {
 
-    @FXML private Button helloWorldButton;
-    @FXML private Button goodByeWorldButton;
-    @FXML private Label label;
+    @FXML private ListView articlesList;
 
     //    @Override
-    public void initialize(URL location, ResourceBundle resourceBundle) {}
-
-    @FXML
-    private void displayHelloWorld() {
-        label.setText("Hello World");
-        helloWorldButton.setVisible(false);
-        if (!goodByeWorldButton.isVisible())
-            goodByeWorldButton.setVisible(true);
+    public void initialize(URL location, ResourceBundle resourceBundle) {
+        generateArticlesList();
     }
 
+    // TODO : Afficher tout les auteurs au survol de la souris
+    // TODO : Générer des vrais articles quand le lecteur XML sera prêt
     @FXML
-    private void goodByeWorld() {
-        label.setText("");
-        goodByeWorldButton.setVisible(false);
-        if (!helloWorldButton.isVisible())
-            helloWorldButton.setVisible(true);
+    private void generateArticlesList() {
+        // XmlReader reader = new XmlReader();
+
+        ObservableList articles = FXCollections.observableArrayList();
+        List<String> authors = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            authors.add("auteur " + i);
+        }
+
+        for (int i = 0; i < 100; i++) {
+            String string = "Titre " + i + "\n\t";
+
+            for (int authorID = 0; authorID < 3; authorID++)
+                string += authors.get(authorID) + ", ";
+
+            if (authors.size() > 3)
+                string += "+" + (authors.size()-3) + " autres";
+
+            articles.add(string);
+        }
+
+        articlesList.setItems(articles);
+
+        /* for (Article article : reader.....) {
+            articlesListText.append(article.)
+        } */
     }
+
 }
