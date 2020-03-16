14a13b03b3a59d58343a86341ce028934c6614db
Airels <yohan13240@hotmail.fr>
2020-03-16 14:19:36 +0100

    Ajout évènement click sur un élement

--- src/main/java/app/arxivorg/controller/ArxivOrgController.java
+++ src/main/java/app/arxivorg/controller/ArxivOrgController.java
@@ -4,12 +4,17 @@ import app.arxivorg.model.Article;
 import app.arxivorg.model.Authors;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -22,6 +27,7 @@ public class ArxivOrgController implements Initializable {
     @FXML private ListView articlesList;
     @FXML private ChoiceBox categoryChoiceBox;
     @FXML private ChoiceBox periodChoiceBox;
+    @FXML private TextFlow articleView;
 
     //    @Override
     public void initialize(URL location, ResourceBundle resourceBundle) {
@@ -30,14 +36,12 @@ public class ArxivOrgController implements Initializable {
         generateArticlesList();
     }
 
-    @FXML
     private void generateCategoryChoiceBox() {
         String categories[] = {"A", "B", "C"};
         categoryChoiceBox.setItems(FXCollections.observableArrayList(categories));
         categoryChoiceBox.setValue("A");
     }
 
-    @FXML
     private void generatePeriodChoiceBox() {
         String periods[] = {"A", "B", "C"};
         periodChoiceBox.setItems(FXCollections.observableArrayList(periods));
@@ -46,7 +50,6 @@ public class ArxivOrgController implements Initializable {
 
     // TODO : Afficher tout les auteurs au survol de la souris
     // TODO : Générer des vrais articles quand le lecteur XML sera prêt
-    @FXML
     private void generateArticlesList() {
         // XmlReader reader = new XmlReader();
 
@@ -76,4 +79,13 @@ public class ArxivOrgController implements Initializable {
         } */
     }
 
+    @FXML
+    public void onMouseClickArticle(MouseEvent event) { // EN DOUBLE CLICK OU SIMPLE CLICK ?
+        if (event.getButton() == MouseButton.PRIMARY) {
+            Text textArticle = new Text(articlesList.getSelectionModel().getSelectedItem().toString());
+
+            articleView.getChildren().clear();
+            articleView.getChildren().add(textArticle);
+        }
+    }
 }
