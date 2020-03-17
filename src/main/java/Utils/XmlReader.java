fd4953bebb420586a2c5308329e30bf1d94cbb83
SAymeric <aymeric.sibiak@etu.univ-amu.fr>
2020-03-17 13:20:52 +0100

    finialisation du Reader

--- src/main/java/Utils/XmlReader.java
+++ src/main/java/Utils/XmlReader.java
@@ -1,4 +1,5 @@
 package Utils;
+
 import java.io.File;
 import java.util.ArrayList;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -6,6 +7,8 @@ import javax.xml.parsers.DocumentBuilder;
 
 import app.arxivorg.model.Article;
 import app.arxivorg.model.Authors;
+import app.arxivorg.model.Category;
+import app.arxivorg.model.SubCategories;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -15,10 +18,12 @@ import org.w3c.dom.Element;
 public class XmlReader
 {
 
-    public static void main (String[] args)
+    public static ArrayList<Article> Reader (File file)
     {
+        ArrayList<Article> articles = new ArrayList<Article>();
+
         try {
-            File inputFile = new File("1.atom");
+            File inputFile = new File(String.valueOf(file));
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
@@ -28,52 +33,57 @@ public class XmlReader
             NodeList secondlist = doc.getElementsByTagName("category");
 
 
-            //ArrayList<Article> articles = new ArrayList<Article>();
+
 
 
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
                 Node primaryNode = primarylist.item(temp);
-                Node secondNode = primarylist.item(temp);
+                Node secondNode = secondlist.item(temp);
 
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    Element aElement = (Element) primaryNode;
-                    Element yElement = (Element) secondNode;
+                    Element entryNode = (Element) nNode;
+                    Element primarycategoryNode = (Element) primaryNode;
+                    Element secondcategoryNode = (Element) secondNode;
 
-                    int lenghtauthor = eElement.getElementsByTagName("author").getLength();
-                    //ArrayList<String> templist = new ArrayList<String>();
+                    int lenghtauthor = entryNode.getElementsByTagName("author").getLength();
+                    ArrayList<String> templist = new ArrayList<String>();
 
                     for (int index = 0 ; index > lenghtauthor ; index ++ ){
-                        //templist.add(eElement.getElementsByTagName("name").item(index).getTextContent());
+                        templist.add(entryNode.getElementsByTagName("name").item(index).getTextContent());
 
                     }
 
-                    //Authors tempauthors = new Authors(templist);
 
-                    //String temptitle = eElement.getElementsByTagName("title").item(0).getTextContent();
+                    Authors tempauthors = new Authors(templist);
 
-                    //String tempcomtent = eElement.getElementsByTagName("summary").item(0).getTextContent();
+                    String temptitle = entryNode.getElementsByTagName("title").item(0).getTextContent();
 
+                    String tempcomtent = entryNode.getElementsByTagName("summary").item(0).getTextContent();
 
+                    ArrayList subcategories = new ArrayList<String>();
 
-
-                    System.out.println("test = "
-                            + aElement
+                    subcategories.add(primarycategoryNode
                             .getAttribute("term"));
 
-                    System.out.println("test second  = "
-                            + yElement
-                            .getAttribute("term"));
+                    SubCategories tempsub = new SubCategories(subcategories);
 
-                    System.out.println(" ");
+
+
+                    Category primalCategory = Category.Computer_Science;
+
+
+                    Article tempArticle = new Article(temptitle, tempauthors, tempcomtent, primalCategory, tempsub );
+                    articles.add(tempArticle);
 
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return articles;
 
     }
 }
