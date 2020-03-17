b9f94eb862df7971c4b531ebaa29ee9a79e832b5
SAymeric <aymeric.sibiak@etu.univ-amu.fr>
2020-03-17 12:28:01 +0100

    Ajout des lecture de balise category

--- src/main/java/Utils/XmlReader.java
+++ src/main/java/Utils/XmlReader.java
@@ -15,47 +15,59 @@ import org.w3c.dom.Element;
 public class XmlReader
 {
 
-    public static ArrayList<Article> ArticlefromXml (File file)
+    public static void main (String[] args)
     {
         try {
-            File inputFile = new File(String.valueOf(file));
+            File inputFile = new File("1.atom");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
             doc.getDocumentElement().normalize();
             NodeList nList = doc.getElementsByTagName("entry");
-            ArrayList<Article> articles = new ArrayList<Article>();
+            NodeList primarylist = doc.getElementsByTagName("arxiv:primary_category");
+            NodeList secondlist = doc.getElementsByTagName("category");
+
+
+            //ArrayList<Article> articles = new ArrayList<Article>();
 
 
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
+                Node primaryNode = primarylist.item(temp);
+                Node secondNode = primarylist.item(temp);
 
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
+                    Element aElement = (Element) primaryNode;
+                    Element yElement = (Element) secondNode;
 
                     int lenghtauthor = eElement.getElementsByTagName("author").getLength();
-                    ArrayList<String> templist = new ArrayList<String>();
+                    //ArrayList<String> templist = new ArrayList<String>();
 
                     for (int index = 0 ; index > lenghtauthor ; index ++ ){
-                        templist.add(eElement.getElementsByTagName("name").item(index).getTextContent());
+                        //templist.add(eElement.getElementsByTagName("name").item(index).getTextContent());
 
                     }
 
-                    Authors tempauthors = new Authors(templist);
+                    //Authors tempauthors = new Authors(templist);
 
-                    String temptitle = eElement.getElementsByTagName("title").item(0).getTextContent();
+                    //String temptitle = eElement.getElementsByTagName("title").item(0).getTextContent();
 
-                    String tempcomtent = eElement.getElementsByTagName("summary").item(0).getTextContent();
-
-
+                    //String tempcomtent = eElement.getElementsByTagName("summary").item(0).getTextContent();
 
 
-                    System.out.println("First Name : "
-                            + eElement
-                            .getElementsByTagName("firstname")
-                            .item(0)
-                            .getTextContent());
+
+
+                    System.out.println("test = "
+                            + aElement
+                            .getAttribute("term"));
+
+                    System.out.println("test second  = "
+                            + yElement
+                            .getAttribute("term"));
+
+                    System.out.println(" ");
 
                 }
             }
