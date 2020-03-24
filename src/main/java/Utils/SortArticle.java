55c0ff1e6a28780c576cda5f263edb42b2cc81bb
SAymeric <aymeric.sibiak@etu.univ-amu.fr>
2020-03-24 13:30:19 +0100

    Ajout de la methode SortbyCategory

--- src/main/java/Utils/SortArticle.java
+++ src/main/java/Utils/SortArticle.java
@@ -1,11 +1,15 @@
 package Utils;
 
 import app.arxivorg.model.Article;
+import app.arxivorg.model.Category;
+
 import java.util.ArrayList;
+
+import static app.arxivorg.model.Category.Computer_Science;
 
 public class SortArticle {
 
-    public static ArrayList<Article> Sortbyauthors (ArrayList<Article> list , String name) {
+    public static ArrayList<Article> SortbyAuthors(ArrayList<Article> list , String name) {
 
         ArrayList<Article> result = new ArrayList<>();
 
@@ -25,8 +29,8 @@ public class SortArticle {
             }
 
 
-            ArrayList<Article> resultleft = Sortbyauthors(leftpart , name);
-            ArrayList<Article> resultright = Sortbyauthors(rightpart, name);
+            ArrayList<Article> resultleft = SortbyAuthors(leftpart , name);
+            ArrayList<Article> resultright = SortbyAuthors(rightpart, name);
 
             result.addAll(resultleft);
             result.addAll(resultright);
@@ -42,12 +46,50 @@ public class SortArticle {
     }
 
 
+    public static ArrayList<Article> SortbyCategory (ArrayList<Article> list , Category type ) {
+
+        ArrayList<Article> result = new ArrayList<>();
+
+        if (list.size() != 1){
+
+            int mid = list.size()/2;
+
+            ArrayList<Article> leftpart = new ArrayList<>();
+            ArrayList<Article> rightpart = new ArrayList<>();
+
+            for (int index = 0 ; index < list.size() ; index++){
+
+                if ( index < mid) leftpart.add(list.get(index));
+
+                else rightpart.add(list.get(index));
+
+            }
+
+
+            ArrayList<Article> resultleft = SortbyCategory(leftpart , type);
+            ArrayList<Article> resultright = SortbyCategory(rightpart, type);
+
+            result.addAll(resultleft);
+            result.addAll(resultright);
+        }
+
+        else {
+                if (type.equals(list.get(0).getCategory())) result.add(list.get(0));
+            }
+
+        return result;
+    }
+
+
+
+
     public static void main(String[] args) {
         ArrayList<Article> authors = XmlReader.read("1.atom");
-        ArrayList<Article> test = Sortbyauthors(authors , "Piji Li");
+        ArrayList<Article> test = SortbyCategory(authors , Computer_Science);
 
 
-        System.out.println(test.get(0).getTitle());
-
+        for (int index = 0 ; index < test.size(); index ++) {
+            System.out.println(test.get(index).getTitle());
+        }
     }
 }
