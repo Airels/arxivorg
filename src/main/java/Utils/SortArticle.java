0354b668f728a190a35103e4452065fe4125a22a
SAymeric <aymeric.sibiak@etu.univ-amu.fr>
2020-03-24 13:50:42 +0100

    Ajout de la methode SortbyDate en commentaire dans l'attente que la date soit ajouter a la classe Article.

--- src/main/java/Utils/SortArticle.java
+++ src/main/java/Utils/SortArticle.java
@@ -2,11 +2,11 @@ package Utils;
 
 import app.arxivorg.model.Article;
 import app.arxivorg.model.Category;
-import app.arxivorg.model.SubCategories;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-
-import static app.arxivorg.model.Category.Computer_Science;
+import java.util.Date;
 
 public class SortArticle {
 
@@ -116,6 +116,45 @@ public class SortArticle {
 
         return result;
     }
+
+    /*public static ArrayList<Article> SortbyDate(ArrayList<Article> list , String date1 , String date2 ) throws ParseException {
+
+        ArrayList<Article> result = new ArrayList<>();
+
+        if (list.size() != 1){
+
+            int mid = list.size()/2;
+
+            ArrayList<Article> leftpart = new ArrayList<>();
+            ArrayList<Article> rightpart = new ArrayList<>();
+
+            for (int index = 0 ; index < list.size() ; index++){
+
+                if ( index < mid) leftpart.add(list.get(index));
+
+                else rightpart.add(list.get(index));
+
+            }
+
+
+            ArrayList<Article> resultleft = SortbyDate(leftpart , date1, date2);
+            ArrayList<Article> resultright = SortbyDate(rightpart, date1 , date2);
+
+            result.addAll(resultleft);
+            result.addAll(resultright);
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date datemin = sdf.parse(date1);
+        Date datemax = sdf.parse(date2);
+        Date datetocompar = sdf.parse(list.get(0).getDate);
+
+        else {
+            if (datemin.compareTo(datetocompar) >= 0 && datemax.compareTo(datetocompar) <= 0) result.add(list.get(0));
+        }
+
+        return result;
+    }*/
 
 
 
