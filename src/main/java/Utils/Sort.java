835891ae80878f9f6fc012c9266e5edfe43c3394
SAymeric <aymeric.sibiak@etu.univ-amu.fr>
2020-03-24 12:54:18 +0100

    Ajout de la classe Sort et ajout de la methode de try qui renvoie une list d'article qui contiennent un auteur précis.

---
+++ src/main/java/Utils/Sort.java
@@ -0,0 +1,56 @@
+package Utils;
+
+import app.arxivorg.model.Article;
+import app.arxivorg.model.Authors;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Sort {
+
+    public static ArrayList<Article> Sortbyauthors (ArrayList<Article> list , String name) {
+
+        ArrayList<Article> result = new ArrayList<>();
+
+        if (list.size() != 1){
+
+            int mid = list.size()/2;
+
+            ArrayList<Article> leftpart = new ArrayList<>();
+            ArrayList<Article> rightpart = new ArrayList<>();
+
+            for (int index = 0 ; index < list.size() ; index++){
+
+                if ( index < mid) leftpart.add(list.get(index));
+
+                else rightpart.add(list.get(index));
+
+            }
+
+
+            ArrayList<Article> resultleft = Sortbyauthors(leftpart , name);
+            ArrayList<Article> resultright = Sortbyauthors(rightpart, name);
+
+            result.addAll(resultleft);
+            result.addAll(resultright);
+        }
+
+        else {
+            for (int index2 = 0 ; index2 < list.get(0).getAuthors().getList().size(); index2 ++){
+                if (name.equals(list.get(0).getAuthors().getList().get(index2))) result.add(list.get(0));
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<Article> authors = XmlReader.read("1.atom");
+        ArrayList<Article> test = Sortbyauthors(authors , "Piji Li");
+
+
+        System.out.println(test.get(0).getTitle());
+
+    }
+}
