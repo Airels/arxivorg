a4d96e1fb6a78944182c7906824e94dbb19f7d1c
SAymeric <aymeric.sibiak@etu.univ-amu.fr>
2020-03-24 13:40:33 +0100

    Ajout de la methode SortbySubcatagories

--- src/main/java/Utils/SortArticle.java
+++ src/main/java/Utils/SortArticle.java
@@ -2,6 +2,7 @@ package Utils;
 
 import app.arxivorg.model.Article;
 import app.arxivorg.model.Category;
+import app.arxivorg.model.SubCategories;
 
 import java.util.ArrayList;
 
@@ -80,12 +81,49 @@ public class SortArticle {
         return result;
     }
 
+    public static ArrayList<Article> SortbySubCategories (ArrayList<Article> list , String sub) {
+
+        ArrayList<Article> result = new ArrayList<>();
+
+        if (list.size() != 1){
+
+            int mid = list.size()/2;
+
+            ArrayList<Article> leftpart = new ArrayList<>();
+            ArrayList<Article> rightpart = new ArrayList<>();
+
+            for (int index = 0 ; index < list.size() ; index++){
+
+                if ( index < mid) leftpart.add(list.get(index));
+
+                else rightpart.add(list.get(index));
+
+            }
+
+
+            ArrayList<Article> resultleft = SortbySubCategories(leftpart , sub);
+            ArrayList<Article> resultright = SortbySubCategories(rightpart, sub);
+
+            result.addAll(resultleft);
+            result.addAll(resultright);
+        }
+
+        else {
+            for (int index2 = 0 ; index2 < list.get(0).getSubCategories().getList().size(); index2 ++){
+                if (sub.equals(list.get(0).getSubCategories().getList().get(index2))) result.add(list.get(0));
+            }
+        }
+
+        return result;
+    }
+
 
 
 
     public static void main(String[] args) {
         ArrayList<Article> authors = XmlReader.read("1.atom");
-        ArrayList<Article> test = SortbyCategory(authors , Computer_Science);
+        ArrayList<Article> test = SortbySubCategories(authors , "cs.LG");
+
 
 
         for (int index = 0 ; index < test.size(); index ++) {
