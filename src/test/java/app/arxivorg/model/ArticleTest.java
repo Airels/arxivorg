e4e59487cce85859cd8c25ae9354a128f8c3d24c
tom <tom.david-broglio@etu.univ>
2020-03-26 16:20:58 +0100

    ajout des test reflettant les changement dans article

--- src/test/java/app/arxivorg/model/ArticleTest.java
+++ src/test/java/app/arxivorg/model/ArticleTest.java
@@ -3,6 +3,7 @@ package app.arxivorg.model;
 import org.junit.jupiter.api.Test;
 
 import javax.swing.plaf.SeparatorUI;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,20 +18,25 @@ public class ArticleTest {
         List<String> auth = new ArrayList<>();
         auth.add("test");
         Authors authors = new Authors(auth);
+        LocalDate date = LocalDate.of(2020,1,1);
         //No Title :
-        assertThrows(IllegalArgumentException.class,()-> new Article("",authors,"content",Category.Economics,new SubCategories(),"link"));
+        assertThrows(IllegalArgumentException.class,()-> new Article("",authors,"content",Category.Economics,new SubCategories(),"link",date));
         //No Content
-        assertThrows(IllegalArgumentException.class,()-> new Article("title",authors,"",Category.Economics,new SubCategories(),"link"));
+        assertThrows(IllegalArgumentException.class,()-> new Article("title",authors,"",Category.Economics,new SubCategories(),"link",date));
         //No authors
-        assertThrows(IllegalArgumentException.class,()-> new Article("title",new Authors(new ArrayList<>()),"content",Category.Economics,new SubCategories(),"link"));
+        assertThrows(IllegalArgumentException.class,()-> new Article("title",new Authors(new ArrayList<>()),"content",Category.Economics,new SubCategories(),"link",date));
         //Null Category
-        assertThrows(IllegalArgumentException.class,()-> new Article("title",authors,"content",null,new SubCategories(),"link"));
+        assertThrows(IllegalArgumentException.class,()-> new Article("title",authors,"content",null,new SubCategories(),"link",date));
         //Null link
-        assertThrows(IllegalArgumentException.class,()-> new Article("title",authors,"content",Category.Physics,new SubCategories(),null));
+        assertThrows(IllegalArgumentException.class,()-> new Article("title",authors,"content",Category.Physics,new SubCategories(),null,date));
+        //Null date
+        assertThrows(IllegalArgumentException.class,()-> new Article("title",authors,"content",Category.Physics,new SubCategories(),"link",null));
+
     }
 
     @Test
     public void GetterTest(){
+
         List<String> auth = new ArrayList<>();
         auth.add("test");
         Authors authors = new Authors(auth);
@@ -38,8 +44,9 @@ public class ArticleTest {
         List<String> subC = new ArrayList<>();
         subC.add("Covid-19");
         SubCategories subCategories = new SubCategories(subC);
+        LocalDate date = LocalDate.of(2020,1,1);
 
-        Article article = new Article("title",authors,"content",Category.Physics,subCategories,"link");
+        Article article = new Article("title",authors,"content",Category.Physics,subCategories,"link",date);
 
         assert (article.getAuthors() == authors);
         assert (article.getCategory() == Category.Physics);
@@ -47,11 +54,15 @@ public class ArticleTest {
         assert (article.getSubCategories() == subCategories);
         assert (article.getTitle().equals("title"));
         assert (article.getLink().equals("link"));
+        assert (article.getDate().equals(date));
 
     }
 
     @Test
     public void SetterTest(){
+
+        LocalDate date = LocalDate.of(2020,1,1);
+        LocalDate dateFalse = LocalDate.of(2020,1,2);
         List<String> auth = new ArrayList<>();
         auth.add("test_false");
         Authors authors_false = new Authors(auth);
@@ -67,7 +78,7 @@ public class ArticleTest {
         SubCategories subCategories = new SubCategories(subCat);
 
 
-        Article article = new Article("title_false",authors_false,"content_false",Category.Economics,subCategories_false,"link_false");
+        Article article = new Article("title_false",authors_false,"content_false",Category.Economics,subCategories_false,"link_false",dateFalse);
 
         article.setAuthors(authors);
         article.setCategory(Category.Physics);
@@ -75,6 +86,7 @@ public class ArticleTest {
         article.setTitle("title");
         article.setSubCategories(subCategories);
         article.setLink("link");
+        article.setDate(date);
 
         assert (article.getAuthors() == authors);
         assert (article.getCategory() == Category.Physics);
@@ -82,6 +94,7 @@ public class ArticleTest {
         assert (article.getSubCategories() == subCategories);
         assert (article.getTitle().equals("title"));
         assert (article.getLink().equals("link"));
+        assert (article.getDate().equals(date));
 
     }
 }
