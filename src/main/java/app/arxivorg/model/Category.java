d5abc3386791ce7b7e1dfc44fec26e9f109f0a15
tom <tom.david-broglio@etu.univ>
2020-03-27 14:20:51 +0100

    modification de l'enum

--- src/main/java/app/arxivorg/model/Category.java
+++ src/main/java/app/arxivorg/model/Category.java
@@ -1,13 +1,25 @@
 package app.arxivorg.model;
 
 public enum Category {
-    All,
-    Physics,
-    Mathematics,
-    Quantitative_Biology,
-    Computer_Science,
-    Quantitative_Finance,
-    Statistics,
-    Electrical_Engineering_and_Systems_Science,
-    Economics
+
+    ALL("ALL"),
+    Physics("physics"),
+    Mathematics("math"),
+    Quantitative_Biology("q-bio"),
+    Computer_Science("cs"),
+    Quantitative_Finance("q_fin"),
+    Statistics("stat"),
+    Electrical_Engineering_and_Systems_Science("eess"),
+    Economics("econ"),
+    quant_ph("quant-ph"),
+    hep_th("hep-th");
+
+    private String name = "";
+    Category(String name){
+        this.name = name;
+    }
+
+    public String toString(){
+        return name;
+    }
 }
