c7fc751288074ca04e34765b73d570192c784492
SAymeric <aymeric.sibiak@etu.univ-amu.fr>
2020-04-02 17:10:54 +0200

    suppresion suite a menace de fonction jger inutile par fifi

--- src/main/java/utils/APICall.java
+++ src/main/java/utils/APICall.java
@@ -10,8 +10,6 @@ import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
 
 public class APICall {
@@ -20,19 +18,6 @@ public class APICall {
             .version(HttpClient.Version.HTTP_2)
             .build();
 
-
-    private static HttpRequest createGet(String typesearch, String searchsubjet){
-
-        String whithourspace = searchsubjet.replace(" ", "+");
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://export.arxiv.org/api/query?search_query=" + typesearch + ":" + whithourspace))
-                .GET()
-                .build();
-
-        return request;
-
-    }
 
     private static HttpRequest createGetFromToAsk(String typesearch, String searchsubjet, int start, int to){
 
@@ -47,35 +32,11 @@ public class APICall {
 
     }
 
-    public static ArrayList<Article> requestApi (String typesearch, String searchsubjet) {
-
-        try {
-
-            HttpResponse<String> response =
-                    httpClient.send(createGet(typesearch, searchsubjet), HttpResponse.BodyHandlers.ofString());
-
-
-            if (response.statusCode() == 200) {
-
-                File requestfile = new File("temp.atom");
-                FileWriter writer = new FileWriter(requestfile);
-                writer.write(response.body());
-                writer.flush();
-
-                ArrayList<Article> requestArticles = XmlReader.read("temp.atom");
-
-                requestfile.delete();
-
-                return requestArticles;
-            }
-        } catch (Exception e) {
-            System.out.println("Request failed");
-        }
-
-        return new ArrayList<Article>();
+    protected static ArrayList<Article> requestApi (String typesearch, String searchsubjet) {
+        return requestApiFromTo(typesearch, searchsubjet, 0 , 100);
     }
 
-    public static ArrayList<Article> requestApiFromTo (String typesearch, String searchsubjet,int start, int to) {
+    protected static ArrayList<Article> requestApiFromTo (String typesearch, String searchsubjet,int start, int to) {
 
         try {
 
