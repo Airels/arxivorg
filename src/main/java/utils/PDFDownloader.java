08dd67ffcb8e8051a8e9de7ee1221ccc965199f3
GrosFlan <guillaume13.nicolai@gmail.com>
2020-04-08 17:03:14 +0100

    création du fichier cible acceuillant le contenu pdf

--- src/main/java/utils/PDFDownloader.java
+++ src/main/java/utils/PDFDownloader.java
@@ -14,12 +14,15 @@ public class PDFDownloader {
      * @throws MalformedURLException
      */
     public static void downloadFile(Article article, File file) {
+
+        File pdfFile = new File(file.getAbsolutePath(), article.getTitle() + ".pdf");
+
         try {
             URL url = new URL(article.getLink());
 
 
             try (BufferedInputStream inputStream = new BufferedInputStream(url.openStream());
-                 FileOutputStream fileOS = new FileOutputStream(file.getAbsolutePath())) {
+                 FileOutputStream fileOS = new FileOutputStream(pdfFile.getAbsolutePath())) {
                 byte data[] = new byte[1024];
                 int byteContent;
                 while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
