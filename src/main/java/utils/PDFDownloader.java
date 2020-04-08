9d59ff80119ed0892a09ac57dfec91fe80277f93
GrosFlan <guillaume13.nicolai@gmail.com>
2020-04-08 15:57:36 +0100

    ne throw plus l'exception

---
+++ src/main/java/utils/PDFDownloader.java
@@ -0,0 +1,38 @@
+package utils;
+
+import app.arxivorg.model.Article;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class PDFDownloader {
+    /**
+     *
+     * @param article
+     * @param file
+     * @throws MalformedURLException
+     */
+    public static void downloadFile(Article article, File file) {
+        try {
+            URL url = new URL(article.getLink());
+
+
+            try (BufferedInputStream inputStream = new BufferedInputStream(url.openStream());
+                 FileOutputStream fileOS = new FileOutputStream(file.getAbsolutePath())) {
+                byte data[] = new byte[1024];
+                int byteContent;
+                while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
+                    fileOS.write(data, 0, byteContent);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }catch(MalformedURLException e){
+            System.out.println("Le fichier n'as pas pu être télécharger");
+        }
+
+
+    }
+}
