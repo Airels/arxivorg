4adf4ef9458abecfd4980d6e34e3198e89fe3959
GrosFlan <guillaume13.nicolai@gmail.com>
2020-04-10 14:48:29 +0100

    retour booleen

--- src/main/java/utils/PDFDownloader.java
+++ src/main/java/utils/PDFDownloader.java
@@ -1,30 +1,29 @@
 package utils;
 
 import app.arxivorg.model.Article;
-import javafx.scene.chart.ScatterChart;
 
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class PDFDownloader {
-    /**
-     * @param article
-     * @param file
-     * @throws MalformedURLException
-     */
-    public static void downloadFile(Article article, File file) {
+
+    public static boolean downloadFile(Article article, File file) {
+        boolean isDownloaded = false;
         try (InputStream in = new URL("https" + article.getLink().substring(4)).openStream()) {
             Files.copy(in,
                     Paths.get(file.getAbsolutePath() + '/'+ article.getTitle() + ".pdf"),
                     StandardCopyOption.REPLACE_EXISTING);
+            isDownloaded = true;
 
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return isDownloaded;
     }
+
 }
