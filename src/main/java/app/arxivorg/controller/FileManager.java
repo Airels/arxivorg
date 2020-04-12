10a2279d55d07f7066dc520a54bece906b4990bc
Airels <yohan13240@hotmail.fr>
2020-04-12 10:43:17 +0200

    Méthode putLine implémenté

--- src/main/java/app/arxivorg/controller/FileManager.java
+++ src/main/java/app/arxivorg/controller/FileManager.java
@@ -1,8 +1,15 @@
 package app.arxivorg.controller;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * FileManager used to simplify read and write file
+ *
+ * @author VIZCAINO Yohan (Airels)
+ */
 public class FileManager {
     private File file;
 
@@ -16,12 +23,23 @@ public class FileManager {
         }
     }
 
-    public void putLine() {
+    public void putLine(String data) {
+        if (!file.canWrite())
+            throw new RuntimeException("Unable to write file");
 
+        try {
+            FileWriter writer = new FileWriter(file, true);
+
+            writer.write(data);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     public String getLines() {
-
+        if (!file.canRead())
+            throw new RuntimeException("Unable to read file");
     }
 
     public String getLine(int line) {
