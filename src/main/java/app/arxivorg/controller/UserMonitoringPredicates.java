25058e25e868a2b784b3698110970f6c440ef03c
Airels <yohan13240@hotmail.fr>
2020-04-17 14:14:49 +0200

    Implémentation méthode ajout de catégorie préférences utilisateur

--- src/main/java/app/arxivorg/controller/UserMonitoringPredicates.java
+++ src/main/java/app/arxivorg/controller/UserMonitoringPredicates.java
@@ -24,15 +24,33 @@ public class UserMonitoringPredicates {
     public static void checkUserMonitoringFile() {
         FileManager fm = new FileManager(fileName);
 
+        // CHECK FILE INTEGRITY
         int lineCategory = fm.getLineEqualsTo("=CATEGORY=");
         int lineAuthors = fm.getLineEqualsTo("=AUTHORS=");
         int lineKeywords = fm.getLineEqualsTo("=KEYWORDS=");
         int lineEND = fm.getLineEqualsTo("=END=");
 
-        if (lineCategory == -1 || lineAuthors == -1 || lineKeywords == -1 || lineEND == -1)
-            fm.wipeFile();
+        if (lineCategory == -1 || lineAuthors == -1 || lineKeywords == -1 || lineEND == -1) {
+            createFile();
+            return;
+        }
 
+        // CHECK CATEGORY INTEGRITY
+        for (Category category : Category.values()) {
+            if (category.equals(Category.All)) continue;
+
+            if (fm.getLineContains(category.getName()) == -1) {
+                createFile();
+                return;
+            }
+        }
+    }
+
+    private static void createFile() {
         // CREATING FILE
+        FileManager fm = new FileManager(fileName);
+        fm.wipeFile();
+
         fm.putLine("=CATEGORY=");
         for (Category category : Category.values()) {
             if (category.equals(Category.All)) continue;
@@ -47,6 +65,18 @@ public class UserMonitoringPredicates {
 
     public static void addCategoryStat(Category category) {
         FileManager fm = new FileManager(fileName);
+        int line = fm.getLineStartsWith(category.getName());
+
+        if (line == -1) {
+            System.err.println("UserMonitoringPredicates error: File altered, recreating...");
+            createFile();
+            addCategoryStat(category);
+            return;
+        }
+
+        int categoryCount = Integer.parseInt(fm.getLine(line).split(" ")[1]); // Line example : cs 3
+
+        fm.putLine(category.getName() + ' ' + (int) (categoryCount+1), line);
     }
 
     public static void addAuthor(String author) {
